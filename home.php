7c3830cd958afc5140caa84e52ca71bd77be64c9
Liam Murray <l.murray8391@gmail.com>
2024-11-20 15:20:28 +0000

    included navbar

--- home.php
+++ home.php
@@ -11,19 +11,5 @@ include('includes/nav.php');
 if (!isset($_SESSION['id'])) {
     require_once('login_tools.php');
 }
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome: <?php echo htmlspecialchars($_SESSION['username']); ?></title>
-</head>
-<body>
-    <h1>Hello <?php echo htmlspecialchars($_SESSION['username']); ?></h1>
-</body>
-</html>
-<?php
 # Include footer file.
 include('includes/footer.php');
-?>
