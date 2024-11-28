b1be273a0aafcf460a104c9f01e9b6490ae8b165
Liam Murray <l.murray8391@gmail.com>
2024-11-28 09:48:00 +0000

    Edited layout of movie listings

--- movie_listing.php
+++ movie_listing.php
@@ -1,25 +1,24 @@
 <?php
+include('includes/nav.php');
 # Open database connection.
 require('connect_db.php');
 # Retrieve movies from 'movie_listing' database table.
 $q = "SELECT * FROM movie_listings";
 $r = mysqli_query($link, $q);
+echo '<div class="row justify-content-center">';
 if (mysqli_num_rows($r) > 0) {
-    # Display body section.
-
+    # Display body section.        
     while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
         echo '
                 <div class="card" style="width: 15rem;margin: 10px ">
-	 	<img src=' . $row['img'] . ' class="card-img-top" alt="T-Shirt">
-	  <div class="card-body">
-	   <h5 class="card-title text-center">' . $row['movie_title'] . '</h5>
-	   <p class="card-text text-center">' . $row['further_info'] . '</p>
-     </div>
-	  <ul class="list-group list-group-flush">
-	  </ul>
-	</div>
+                    <img src=' . $row['img'] . ' class="card-img-top" alt="T-Shirt">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">' . $row['movie_title'] . '</h5>
+                        <p class="card-text text-center">' . $row['further_info'] . '</p>
+                    </div>
+	            </div>       
 	';
-    }
+    }          
     # Close database connection.
     mysqli_close($link);
 }
@@ -28,3 +27,5 @@ else {
     echo '<p>There are currently no movies showing.</p>
 	';
 }
+echo '</div>'; 
+include('includes/footer.php');
