d23761f403316921a0f729ae2d3a55eeee7a32bb
Liam Murray <l.murray8391@gmail.com>
2024-11-29 15:40:09 +0000

    Include for filter.php

--- movie_listing.php
+++ movie_listing.php
@@ -5,6 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 include('includes/nav.php');
+include('includes/filter.php');
 # Open database connection.
 require('connect_db.php');
 # Retrieve movies from 'movie_listing' database table.
