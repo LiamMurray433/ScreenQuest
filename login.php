637bf7982e6ed17c5d08feebd1c2ba289eff6bd5
Liam Murray <l.murray8391@gmail.com>
2024-12-04 15:42:11 +0000

    Styling login form

--- login.php
+++ login.php
@@ -2,30 +2,37 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Login</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>CRUD Practice!</title>
+    <link rel="stylesheet" href="main.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
+        crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>Login</h1>
-    <form action="login_action.php" method="post">
-        <label for="email">Email:</label><br>
-        <input type="text"
-            class="form-control"
-            placeholder="Email"
-            name="email"
-            required>
-        <br><br>
-        <label for="password">Password:</label><br>
-        <input type="password"
-            class="form-control"
-            placeholder="Password"
-            name="password"
-            required>
-        <br><br>
-        <input type="submit" value="Login">
-    </form>
+    <div class="login-wrapper">
+        <div class="login-container">
+            <h1 class="login-title">Login</h1>
+            <form action="login_action.php" method="post">
+                <div class="mb-3 form-input">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" placeholder="Email" name="email" required>
+                </div>
+                <div class="mb-3 form-input">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" placeholder="Password" name="password" required>
+                </div>
+                <br><br>
+                <input type="submit" value="Login">
+            </form>
+        </div>
+    </div>
 </body>
 
 </html>
