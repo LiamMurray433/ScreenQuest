fc9858ea0191273749de3fbbc845ef1621d27729
Liam Murray <l.murray8391@gmail.com>
2024-12-04 15:41:55 +0000

    Modified head section

--- show2.php
+++ show2.php
@@ -1,5 +1,6 @@
 <?php
 include('includes/nav.php');
+include('includes/filter.php')
 ?>
 <!doctype html>
 <html lang="en">
@@ -7,12 +8,15 @@ include('includes/nav.php');
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>CRUD Practice!</title>
+    <link rel="stylesheet" href="main.css">
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <title>Movies</title>
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
+        crossorigin="anonymous">
 </head>
 
 <body>
