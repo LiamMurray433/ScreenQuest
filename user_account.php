107fd098854597999e52614458fba3d25f7d52c0
Liam Murray <l.murray8391@gmail.com>
2024-12-06 20:09:24 +0000

    Redesigned user page

--- user_account.php
+++ user_account.php
@@ -39,16 +39,51 @@ if (mysqli_num_rows($r) > 0) {
         $year = substr($date, 0, 4);
 
         echo '	
-	  <h1>'  . $row['username'] . ' </h1>
-	  <hr>
-	  User ID : EC2024/'  . $row['id'] . ' 
-	  <hr>
-	  Email :  ' . $row['email'] . '
-	  <hr>
-	  Registration Date : ' . $day . '/' . $month . '/' . $year . '  
-	  <hr>
-    </body>
-    </html>';
+        <section>
+            <div class="container py-5 account-container" style="background-color: #648381;">
+             <h1>'  . $row['username'] . ' </h1>
+                <div class="col-lg-8">
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <p class="mb-0">First Name</p>
+                            </div>
+                            <div class="col-sm-9">
+                            <p class="text-muted mb-0">'. $row['first_name'] . '</div></p>
+                        </div>
+                        <hr>
+                         <div class="row">
+                            <div class="col-sm-3">
+                            <p class="mb-0">Surname</p>
+                            </div>
+                        <div class="col-sm-9">
+                        <p class="text-muted mb-0">' . $row['last_name'].'</p>
+                        </div>
+                        </div>
+                        <hr>
+                         <div class="row">
+                            <div class="col-sm-3">
+                            <p class="mb-0">Email</p>
+                            </div>
+                        <div class="col-sm-9">
+                        <p class="text-muted mb-0">' . $row['email'].'</p>
+                        </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-3">
+                            <p class="mb-0">Member Since</p>
+                            </div>
+                        <div class="col-sm-9">
+                        <p class="text-muted mb-0"> ' . $day . '/' . $month . '/' . $year . '</p>
+                        </div>
+                        </div>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        </section>';
     }
 
     # Close database connection.
