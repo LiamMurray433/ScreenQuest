e679f331dd1be38e0df19dc4199262988e49ca4f
Liam Murray <l.murray8391@gmail.com>
2024-12-06 19:55:37 +0000

    removed include filter.php

--- movie.php
+++ movie.php
@@ -5,7 +5,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include('includes/nav.php');
-include('includes/filter.php');
 # DISPLAY COMPLETE LOGGED IN PAGE.
 
 ?>
