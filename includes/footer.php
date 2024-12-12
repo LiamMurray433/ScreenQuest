57cda1420c7568a2602f4072148e2415e2ddf29e
Liam Murray <l.murray8391@gmail.com>
2024-12-12 13:29:03 +0000

    removed head from footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,59 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>CRUD Practice!</title>
-    <link rel="stylesheet" href="main.css">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
-        crossorigin="anonymous">
-</head>
-
-
-    <footer style="width: 100%; background-color: #8A3033; color: #ecf0f1; padding: 20px 0; text-align: center;">
-        <div style="width: 100%; margin: 0 auto; display: flex; flex-wrap: wrap; justify-content: space-between; align-items: center;">
-            <div style="flex: 1 1 200px; text-align: left; margin: 10px;">
-                <h4 style="margin: 0; font-size: 18px;">About Us</h4>
-                <p style="margin: 5px 0; font-size: 14px; line-height: 1.5;">
-                    We are passionate about bringing the best experiences through technology.
-                </p>
-            </div>
-            <div style="flex: 1 1 200px; text-align: left; margin: 10px;">
-                <h4 style="margin: 0; font-size: 18px;">Quick Links</h4>
-                <ul style="list-style: none; padding: 0; margin: 5px 0; font-size: 14px;">
-                    <li><a href="#" style="color: #1abc9c; text-decoration: none;">Home</a></li>
-                    <li><a href="#" style="color: #1abc9c; text-decoration: none;">Services</a></li>
-                    <li><a href="#" style="color: #1abc9c; text-decoration: none;">Contact</a></li>
-                    <li><a href="#" style="color: #1abc9c; text-decoration: none;">Privacy Policy</a></li>
-                </ul>
-            </div>
-            <div style="flex: 1 1 200px; text-align: left; margin: 10px;">
-                <h4 style="margin: 0; font-size: 18px;">Follow Us</h4>
-                <div style="margin: 10px 0;">
-                    <a href="#" style="color: #ecf0f1; text-decoration: none; margin-right: 10px;">Facebook</a>
-                    <a href="#" style="color: #ecf0f1; text-decoration: none; margin-right: 10px;">Twitter</a>
-                    <a href="#" style="color: #ecf0f1; text-decoration: none;">Instagram</a>
-                </div>
+<footer style="width: 100%; background-color: #8A3033; color: #ecf0f1; padding: 20px 0; text-align: center;">
+    <div style="width: 100%; margin: 0 auto; display: flex; flex-wrap: wrap; justify-content: space-between; align-items: center;">
+        <div style="flex: 1 1 200px; text-align: left; margin: 10px;">
+            <h4 style="margin: 0; font-size: 18px;">About Us</h4>
+            <p style="margin: 5px 0; font-size: 14px; line-height: 1.5;">
+                We are passionate about bringing the best experiences through technology.
+            </p>
+        </div>
+        <div style="flex: 1 1 200px; text-align: left; margin: 10px;">
+            <h4 style="margin: 0; font-size: 18px;">Quick Links</h4>
+            <ul style="list-style: none; padding: 0; margin: 5px 0; font-size: 14px;">
+                <li><a href="#" style="color: #1abc9c; text-decoration: none;">Home</a></li>
+                <li><a href="#" style="color: #1abc9c; text-decoration: none;">Services</a></li>
+                <li><a href="#" style="color: #1abc9c; text-decoration: none;">Contact</a></li>
+                <li><a href="#" style="color: #1abc9c; text-decoration: none;">Privacy Policy</a></li>
+            </ul>
+        </div>
+        <div style="flex: 1 1 200px; text-align: left; margin: 10px;">
+            <h4 style="margin: 0; font-size: 18px;">Follow Us</h4>
+            <div style="margin: 10px 0;">
+                <a href="#" style="color: #ecf0f1; text-decoration: none; margin-right: 10px;">Facebook</a>
+                <a href="#" style="color: #ecf0f1; text-decoration: none; margin-right: 10px;">Twitter</a>
+                <a href="#" style="color: #ecf0f1; text-decoration: none;">Instagram</a>
             </div>
         </div>
-        <div style="margin-top: 20px; font-size: 14px;">
-            &copy; 2024 YourCompanyName. All rights reserved.
-        </div>
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"></script>
+    </div>
+    <div style="margin-top: 20px; font-size: 14px;">
+        &copy; 2024 YourCompanyName. All rights reserved.
+    </div>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 
 </html>
