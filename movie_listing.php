1a836cca6f443f5f1ce37340d167e722446b0280
Liam Murray <l.murray8391@gmail.com>
2024-12-12 23:34:37 +0000

    wrapper div

--- movie_listing.php
+++ movie_listing.php
@@ -36,27 +36,30 @@ require('connect_db.php');
 # Retrieve movies from 'movie_listing' database table.
 $q = "SELECT * FROM movie_listings";
 $r = mysqli_query($link, $q);
-echo '<div class="row justify-content-center" style="background-color:#000F08;">';
-if (mysqli_num_rows($r) > 0) {
-    # Display body section.        
-    while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
-        echo '
-                <div class="card movie-card" style="width: 15rem;margin: 10px ">
-                    <img src=' . $row['img'] . ' class="card-img-top" alt="T-Shirt">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">' . $row['movie_title'] . '</h5>
-                        <a href="movie.php?movie_id=' . $row['movie_id'] . '" class="btn btn-secondary btn-block" role="button">Book Now</a>
-                    </div>
-	            </div>       
-	';
+echo '
+<div class="movie-wrapper">
+    <div class="row justify-content-center custom-movie-listings" style="background-color:#000F08;">';
+    if (mysqli_num_rows($r) > 0) {
+        # Display body section.        
+        while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+            echo '
+                    <div class="card movie-card" style="width: 15rem;margin: 10px ">
+                        <img src=' . $row['img'] . ' class="card-img-top" alt="T-Shirt">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">' . $row['movie_title'] . '</h5>
+                            <a href="movie.php?movie_id=' . $row['movie_id'] . '" class="btn btn-secondary btn-block" role="button">Book Now</a>
+                        </div>
+                    </div>       
+        ';
+        }
+        # Close database connection.
+        mysqli_close($link);
     }
-    # Close database connection.
-    mysqli_close($link);
-}
-# Or display message.
-else {
-    echo '<p>There are currently no movies showing.</p>
-	';
-}
-echo '</div>';
+    # Or display message.
+    else {
+        echo '<p>There are currently no movies showing.</p>
+        ';
+    }
+    echo '</div>
+     </div>';
 include('includes/footer.php');
