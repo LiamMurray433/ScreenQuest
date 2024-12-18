9c1b0f4122ae2c82c31995022db54a5b44ef1987
Liam Murray <l.murray8391@gmail.com>
2024-12-18 12:27:30 +0000

    cleaned up checkout

--- checkout.php
+++ checkout.php
@@ -1,3 +1,21 @@
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>ScreenQuest</title>
+    <link rel="stylesheet" href="main.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
+        crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+
 <?php # DISPLAY COMPLETE REGISTRATION PAGE.
 # Access session.
 session_start();
@@ -45,7 +63,7 @@ if (isset($_GET['total']) && ($_GET['total'] > 0) && (!empty($_SESSION['cart']))
 
     # Display order number.
     echo '
-<div class="container">
+<div class="container justify-content-center">
 	
 	';
     # Remove cart items.  
@@ -69,7 +87,9 @@ LIMIT 1";
 $r = mysqli_query($link, $q);
 if (mysqli_num_rows($r) > 0) {
 
-    echo '<img width="256" class="img-fluid" alt="QR Code " src="img\qrcode.jpeg">';
+    echo '<div class="container justify-content-center" style="margin: 20px;">
+    
+    <img width="256" class="img-fluid" alt="QR Code " src="img\qrcode.jpeg">';
 
     while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
         $date = $row["booking_date"];
@@ -77,13 +97,13 @@ if (mysqli_num_rows($r) > 0) {
         $month = substr($date, 5, 2);
         $year = substr($date, 0, 4);
         echo '
-        <div class="container">
+        <div class="container column justify-content-center">
 		<p>Booking Reference:  #EC1000' . $row['booking_id'] . '</p> 
 		<p>Total Paid:   &pound ' . $row['total'] . ' </p>
 		<p>' . $day . '/' . $month . '/' . $year . '.  <p>
         </div>
 				
-';
+        </div>';
     }
 
     # Close database connection.
